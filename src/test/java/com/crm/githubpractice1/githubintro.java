02a4a4554d5193490cc1dbd8a04ee4361e5bb5bf
Dell <Dell@DESKTOP-5CHFJML>
2021-12-09 15:12:20 +0530

    push and commit

--- src/test/java/com/crm/githubpractice1/githubintro.java
+++ src/test/java/com/crm/githubpractice1/githubintro.java
@@ -3,12 +3,14 @@ package com.crm.githubpractice1;
 import org.testng.annotations.Test;
 
 public class githubintro {
-	public class Githubintro {
+
 		
 		@Test
 		public void githubintro() {
 			System.out.println("PUSH AND COMMIT");
-		}
+			System.out.println("Pull");
+			System.out.println("hurray");
+	
 		
 
 	}
