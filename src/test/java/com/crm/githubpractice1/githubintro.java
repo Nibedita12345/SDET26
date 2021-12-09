321efccb56dcaf0d678d8036b831d720b9fe51e6
Dell <Dell@DESKTOP-5CHFJML>
2021-12-09 14:05:00 +0530

    committed

---
+++ src/test/java/com/crm/githubpractice1/githubintro.java
@@ -0,0 +1,17 @@
+package com.crm.githubpractice1;
+
+import org.testng.annotations.Test;
+
+public class githubintro {
+	public class Githubintro {
+		
+		@Test
+		public void githubintro() {
+			System.out.println("PUSH AND COMMIT");
+		}
+		
+
+	}
+
+
+}
